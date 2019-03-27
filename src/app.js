3545db70d04009c066da5779c81f7669bda4d436
Aqib Javed <aqibjaved50@gmail.com>
2019-03-27 16:31:47 +0500

    React-Native Login with Firebase- first commit

---
+++ src/app.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { View } from "react-native";
+import firebase from "firebase";
+import { Header, Button, Spinner } from "./components/common";
+import LoginForm from "./components/LoginForm";
+
+class App extends Component {
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBNRXpNTbcaQ6cJ-j33yGxvtZ-PIQWkqyI",
+      authDomain: "login-app-ec02c.firebaseapp.com",
+      databaseURL: "https://login-app-ec02c.firebaseio.com",
+      storageBucket: "login-app-ec02c.appspot.com",
+      messagingSenderId: "649904699304"
+    });
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        {this.renderContent()}
+      </View>
+    );
+  }
+}
+
+export default App;
